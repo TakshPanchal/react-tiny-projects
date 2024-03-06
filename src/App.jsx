8fcac6555e420e6b7faf5ad13869607ac9bfecc0
taksh <takshtax@gmail.com>
2024-03-06 10:26:05 +0530

    project restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import Accordian from './Accordian'
-import data from './data'
+import Accordian from "./components/accordian/Accordian";
+import data from "./components/accordian/data";
 
 function App() {
-
   return (
-   <div>
-    <Accordian 
-    data={data}/>
-   </div>
-  )
+    <div>
+      <Accordian data={data} />
+    </div>
+  );
 }
 
-export default App
+export default App;
