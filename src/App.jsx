e5fa5ad54245919034ae9cce64760882628379f0
taksh <takshtax@gmail.com>
2024-03-06 10:34:38 +0530

    started random color project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Accordian from "./components/accordian/Accordian";
 import data from "./components/accordian/data";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <div>
       <Accordian data={data} />
+      <RandomColor />
     </div>
   );
 }
---
+++ src/components/random-color/index.jsx
@@ -0,0 +1,13 @@
+const RandomColor = () => {
+  return (
+    <div className="warpper">
+      <div className="buttons">
+        <button className="btn"> Create HEX Color</button>
+        <button className="btn">Create RGB Color</button>
+        <button className="btn">Create Random Color</button>
+      </div>
+    </div>
+  );
+};
+
+export default RandomColor;
